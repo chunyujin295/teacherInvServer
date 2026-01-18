2034a57c0d3be508c10b07d9982ee7fcf630ab3f
chenyujin <3029795434@qq.com>
2026-01-18 22:44:22 +0800

    feat: 增加基础数据结构

--- cmake/insert3rd.cmake
+++ cmake/insert3rd.cmake
@@ -19,6 +19,7 @@ copy_qt_libs(${CMAKE_RUNTIME_OUTPUT_DIRECTORY} QT_LIBS
 #        Designer
         Network
         Sql
+        HttpServer
 )
 
 copy_dir_files(${CMAKE_SOURCE_DIR}/3rdparty/db ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} )
